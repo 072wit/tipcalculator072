b3809d058b1c4caaaadd9fe65a523769ecee5d95
072sirawit <ailpki12s@gmail.com>
2026-02-11 06:10:52 +0700

    first commit tip calculator

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,72 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [bill, setBill] = useState<number>(0);
+  const [tipPercent, setTipPercent] = useState<number>(0);
+  const [tipTotal, setTipTotal] = useState<number>(0);
+  const [billTotal, setBillTotal] = useState<number>(0);
+
+  const calculate = () => {
+    const tip = bill * (tipPercent / 100);
+    setTipTotal(tip);
+    setBillTotal(bill + tip);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="min-h-screen bg-white flex flex-col items-center p-10 gap-6">
+      <h1 className="text-4xl font-bold text-blue-600">Tip Calculator</h1>
+
+     
+      <div className="flex items-center gap-4">
+        <h2 className="text-5xl font-bold">bill</h2>
+        <input
+          type="number"
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+          className="bg-yellow-300 text-center text-2xl font-bold p-3 w-40"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+
+      <div className="flex items-center gap-6">
+        <h2 className="text-5xl font-bold">Tip</h2>
+
+        <button
+          onClick={() => setTipPercent(5)}
+          className={`p-6 text-xl w-32 ${
+            tipPercent === 5 ? "bg-blue-400" : "bg-blue-200"
+          }`}
+        >
+          5%
+        </button>
+      </div>
+
+      <button
+        onClick={calculate}
+        className="bg-teal-200 px-10 py-4 text-xl font-bold rounded"
+      >
+        Calculate
+      </button>
+
+      <div className="mt-6 text-2xl flex flex-col gap-3">
+        <p>
+          Tip Total:{" "}
+          <span className="bg-pink-200 px-6 py-2">{tipTotal.toFixed(2)}</span>
+        </p>
+
+        <p>
+          Bill Total:{" "}
+          <span className="bg-pink-200 px-6 py-2">{billTotal.toFixed(2)}</span>
+        </p>
+        <a
+  href="https://github.com/072wit"
+  target="_blank"
+  className="mt-6 text-blue-600 underline"
+>
+  🔗 ดูโค้ดบน GitHub
+</a>
+      </div>
     </div>
   );
 }
